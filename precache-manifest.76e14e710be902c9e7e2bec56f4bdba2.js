a946ba77d36de7a1eacf35516a59fc5e8ded07a9
Sandra Gomez <sandra.gomez@mimacom.com>
2019-03-14 16:55:02 +0100

    Updates

---
+++ precache-manifest.76e14e710be902c9e7e2bec56f4bdba2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc6d0012cf5013135d77",
+    "url": "/sanders-vag/es-guide/static/js/runtime~main.918872b7.js"
+  },
+  {
+    "revision": "c0d55d57b8fd2e28a036",
+    "url": "/sanders-vag/es-guide/static/js/main.a394c1c5.chunk.js"
+  },
+  {
+    "revision": "4c5a8539bc6b4c5bd5e2",
+    "url": "/sanders-vag/es-guide/static/js/2.c82c6027.chunk.js"
+  },
+  {
+    "revision": "c0d55d57b8fd2e28a036",
+    "url": "/sanders-vag/es-guide/static/css/main.5616d249.chunk.css"
+  },
+  {
+    "revision": "1ac89a6a72b8e630f50f48339c51e63e",
+    "url": "/sanders-vag/es-guide/index.html"
+  }
+];
