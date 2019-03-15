2277a027bd829ff28fe55dee197233abf534de2f
Sandra Gomez <sandra.gomez@mimacom.com>
2019-03-15 11:35:59 +0100

    Improved conditional rendering

--- src/components/section-item/SectionItem.js
+++ src/components/section-item/SectionItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactMarkdown from "react-markdown";
 
 import Codepen from "../codepen/Codepen";
@@ -6,29 +6,24 @@ import Codepen from "../codepen/Codepen";
 import "./SectionItem.css";
 
 const SectionItem = ({ data }) => {
-  if (!data.exampleHash) {
-    return (
-      <div>
-        <a name={data.tag}><h3>{data.title}</h3></a>
-        <hr />
-        <h4>Description</h4>
-        <ReactMarkdown source={data.description} />
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <a name={data.tag}><h3>{data.title}</h3></a>
-        <hr />
-        <h4>Description</h4>
-        <ReactMarkdown source={data.description} />
-        <h4>Example</h4>
-        <div className="example">
-          <Codepen hash={data.exampleHash} name={data.title} />
-        </div>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <a name={data.tag}>
+        <h3>{data.title}</h3>
+      </a>
+      <hr />
+      <h4>Description</h4>
+      <ReactMarkdown source={data.description} />
+      {data.exampleHash && (
+        <Fragment>
+          <h4>Example</h4>
+          <div className="example">
+            <Codepen hash={data.exampleHash} name={data.title} />
+          </div>
+        </Fragment>
+      )}
+    </div>
+  );
 };
 
 export default SectionItem;
