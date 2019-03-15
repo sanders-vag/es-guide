94284c821f09230d2e95030b624c8869dcda619b
Sandra Gomez <sandra.gomez@mimacom.com>
2019-03-15 11:51:45 +0100

    added scroll up button

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
-import Container from 'react-bootstrap/Container'
+import Container from "react-bootstrap/Container";
+import ScrollUpButton from "react-scroll-up-button";
 
-import Toc from '../toc/Toc'
-import Section from '../section/Section'
-
+import Toc from "../toc/Toc";
+import Section from "../section/Section";
 
 import "./App.css";
-import data from './sources.json'
+import data from "./sources.json";
 
 class App extends Component {
-
   render() {
-    const dataMap = new Map(Object.entries(data))
+    const dataMap = new Map(Object.entries(data));
     const arrayValues = [...dataMap.values()];
     return (
       <Container>
+        <ScrollUpButton />
         <h1>From ES2015 to ESNext</h1>
-        <Toc sourceMap={dataMap}/>
+        <Toc sourceMap={dataMap} />
         <Section data={arrayValues} />
       </Container>
     );
